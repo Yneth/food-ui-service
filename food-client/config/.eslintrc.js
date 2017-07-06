55d3643486edef140add01860329ef262baa5252
xrocketmanx <xrocketmanx007@gmail.com>
2017-07-06 23:34:26 +0300

    feature-ui-library: update eslint
    -jsx prop indentation is 4
    -increment/decrement is allowed

--- food-client/config/.eslintrc.js
+++ food-client/config/.eslintrc.js
@@ -12,8 +12,10 @@ module.exports = {
     },
     "rules": {
         "linebreak-style": "off",
+        "no-plusplus": "off",
         "indent": ["error", 4],
         "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
         "react/require-default-props": "off",
         "no-trailing-spaces": ["error", { "skipBlankLines": true }]
     },
