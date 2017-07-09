777f16676da001b207eb07072267f7445eddca85
Oleksandr_Amirov <xrocketmanx007@gmail.com>
2017-07-09 11:48:15 +0300

    feature-auth: extend configureActions with async actionCreators

--- food-client/src/services/configureActions.js
+++ food-client/src/services/configureActions.js
@@ -1,15 +1,45 @@
-const configureActions = (prefix = '') => (type, creator) => {
+export const getAsyncTypes = type => (
+    {
+        request: `${type}_REQUEST`,
+        success: `${type}_SUCCESS`,
+        failure: `${type}_FAILURE`,
+    }
+);
+
+const configureActions = (prefix = '') => (type, interceptor) => {
     const prefixedType = prefix + type;
 
     const actionCreator = (...payload) => (
         {
             type: prefixedType,
-            payload: creator ? creator(...payload) : payload[0],
+            payload: interceptor ? interceptor(...payload) : payload[0],
         }
     );
 
     actionCreator.type = prefixedType;
 
+    const asyncTypes = getAsyncTypes(prefixedType);
+    const asyncCreator = (...payload) => (dispatch) => {
+        dispatch({
+            type: asyncTypes.request,
+        });
+
+        const promiseCreator = interceptor || (arg => Promise.resolve(arg));
+
+        return promiseCreator(...payload)
+            .then((result) => {
+                if (!result.error) {
+                    dispatch({ type: asyncTypes.success, result });
+                } else {
+                    dispatch({ type: asyncTypes.failure, error: result.error });
+                }
+            })
+            .catch(error => dispatch({ type: asyncTypes.failure, error }));
+    };
+
+    asyncCreator.type = asyncTypes;
+
+    actionCreator.async = asyncCreator;
     return actionCreator;
 };
 
