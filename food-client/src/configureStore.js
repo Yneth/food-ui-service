6282fcba39afacedd8f2e7319f00e5166b2491fe
xrocketmanx <xrocketmanx007@gmail.com>
2017-08-01 19:31:34 +0300

    feature-mediator: add mediator as middleware

--- food-client/src/configureStore.js
+++ food-client/src/configureStore.js
@@ -1,15 +1,19 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import Mediator from 'services/mediator';
+import createMiddleware from 'services/mediator/middleware';
 import rootReducer from './rootReducer';
 
 const configureStore = (initialState) => {
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+    const mediator = new Mediator();
+
     const store = createStore(
         rootReducer,
         initialState,
         composeEnhancers(
-            applyMiddleware(thunk),
+            applyMiddleware(thunk, createMiddleware(mediator)),
         ),
     );
 
