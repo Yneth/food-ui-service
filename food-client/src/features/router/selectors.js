4fcefa77b7e36af9ffed3c42fc9ecb8c36ca88b3
xrocketmanx <xrocketmanx007@gmail.com>
2017-08-03 14:04:16 +0300

    feature-router: make selector safe

--- food-client/src/features/router/selectors.js
+++ food-client/src/features/router/selectors.js
@@ -2,4 +2,7 @@ import { ROUTER_KEY } from './constants';
 
 export const selectRouterData = state => state[ROUTER_KEY];
 
-export const selectMatch = (state, name) => selectRouterData(state).matches[name];
+export const selectMatch = (state, name) => {
+    const matches = selectRouterData(state).matches;
+    return matches ? matches[name] : null;
+};
